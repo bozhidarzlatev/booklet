e55e91b42521b43ee5d4106824783c0bc7193fad
bozhidarzlatev <156793066+bozhidarzlatev@users.noreply.github.com>
2024-11-26 11:10:49 +0200

    CSS fices

--- booklet-app/src/app/app.routes.ts
+++ booklet-app/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
+import { AddComponent } from './add/add.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { CartComponent } from './user/cart/cart.component';
+import { RegisterComponent } from './user/register/register.component';
+import { LoginComponent } from './user/login/login.component';
+import { MainComponent } from './main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'add', component: AddComponent},
+    {path: 'all', component: MainComponent},
+    {path: 'profile', component: ProfileComponent},
+    {path: 'cart', component: CartComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: '404', component: ErrorComponent},
+    {path: '**', redirectTo: '/404', pathMatch: 'full'}
+
+];
